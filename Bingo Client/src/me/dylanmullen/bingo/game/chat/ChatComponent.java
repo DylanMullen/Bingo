ad3ee997b04dd9911a9e307db11e3843e4eb4d30
Dylan Mullen <31774413+DylanMullen@users.noreply.github.com>
2020-06-21 02:15:38 +0100

    Sorted out autoscrolling after update.
    TODO: Make autoscrolling dependent on a user setting.

--- Bingo Client/src/me/dylanmullen/bingo/game/chat/ChatComponent.java
+++ Bingo Client/src/me/dylanmullen/bingo/game/chat/ChatComponent.java
@@ -1,51 +1,178 @@
 package me.dylanmullen.bingo.game.chat;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.JTextArea;
+import javax.swing.JComponent;
+import javax.swing.JScrollPane;
+import javax.swing.Scrollable;
+import javax.swing.SwingConstants;
+
+import me.dylanmullen.bingo.window.ui.UIColour;
 
 /**
  * @author Dylan
  * @date 19 Jun 2020
  * @project Bingo Client
  */
-public class ChatComponent extends JTextArea
+public class ChatComponent extends JComponent implements Scrollable, MouseMotionListener
 {
 
-	private static final long serialVersionUID = 2277345570147389600L;
+	private static final long serialVersionUID = 6717389248920350598L;
 
+	private JScrollPane scrollPanel;
 	private List<String> chatMessages;
 
-	public ChatComponent(int x, int y, int width, int height)
+	private boolean updateScrollbar;
+	private int maxUnitIncrement = 20;
+
+	public ChatComponent(JScrollPane scroll, int x, int y, int width, int height)
 	{
 		setBounds(x, y, width, height);
+		this.scrollPanel = scroll;
 		this.chatMessages = new ArrayList<String>();
+		this.updateScrollbar = false;
 		setup();
 	}
 
 	private void setup()
 	{
-		setEditable(false);
 	}
 
-	public void addMessage(String message)
+	public void addMessage(JScrollPane scroll, String message)
 	{
 		if (getChatMessages().size() > 500)
 			getChatMessages().remove(0);
 
-		getChatMessages().add(message);
-		updateTextArea(message);
+		getChatMessages().add("tes");
+		repaint();
 	}
 
-	private void updateTextArea(String data)
+	@Override
+	protected void paintComponent(Graphics g)
 	{
-		append(data + "\n");
+		Graphics2D g2 = (Graphics2D)g;
+		drawMessages(g2);
+		handleSizingChanges();
+	}
+
+	private void drawMessages(Graphics2D g2)
+	{
+		g2.setColor(UIColour.FRAME_BINGO_BG_TOP.toColor());
+		g2.fillRect(0, 0, getWidth(), getHeight());
+		for (int i = 0; i < getChatMessages().size(); i++)
+		{
+			g2.setColor((i % 2 == 0 ? Color.WHITE : Color.black));
+			g2.fillRect(0, i * 50, getWidth(), 50);
+		}
+	}
+	
+	private void handleSizingChanges()
+	{
+		int height = getChatMessages().size() * 50;
+
+		if (getHeight() < height)
+		{
+			setBounds(getX(), getY(), getWidth(), height);
+			this.updateScrollbar = true;
+			return;
+		}
+		if (shouldUpdateScrollbar())
+		{
+			getScrollPanel().getVerticalScrollBar().setValue(height);
+			this.updateScrollbar = false;
+		}
 	}
 
 	public List<String> getChatMessages()
 	{
 		return this.chatMessages;
+	}
+
+	public JScrollPane getScrollPanel()
+	{
+		return this.scrollPanel;
+	}
+
+	private boolean shouldUpdateScrollbar()
+	{
+		return this.updateScrollbar;
+	}
+
+	// Scrollable
+	public void mouseMoved(MouseEvent e)
+	{
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e)
+	{
+		Rectangle r = new Rectangle(e.getX(), e.getY(), 1, 1);
+		scrollRectToVisible(r);
+	}
+
+	@Override
+	public Dimension getPreferredSize()
+	{
+		return new Dimension(getWidth(), getHeight());
+	}
+
+	public Dimension getPreferredScrollableViewportSize()
+	{
+		return getPreferredSize();
+	}
+
+	public int getScrollableUnitIncrement(Rectangle visibleRect, int orientation, int direction)
+	{
+		int currentPosition = 0;
+		if (orientation == SwingConstants.HORIZONTAL)
+		{
+			currentPosition = visibleRect.x;
+		} else
+		{
+			currentPosition = visibleRect.y;
+		}
+		if (direction < 0)
+		{
+			int newPosition = currentPosition - (currentPosition / this.maxUnitIncrement) * this.maxUnitIncrement;
+			return (newPosition == 0) ? this.maxUnitIncrement : newPosition;
+		} else
+		{
+			return ((currentPosition / this.maxUnitIncrement) + 1) * this.maxUnitIncrement - currentPosition;
+		}
+	}
+
+	public int getScrollableBlockIncrement(Rectangle visibleRect, int orientation, int direction)
+	{
+		if (orientation == SwingConstants.HORIZONTAL)
+		{
+			return visibleRect.width - this.maxUnitIncrement;
+		} else
+		{
+			return visibleRect.height - this.maxUnitIncrement;
+		}
+	}
+
+	public boolean getScrollableTracksViewportWidth()
+	{
+		return false;
+	}
+
+	public boolean getScrollableTracksViewportHeight()
+	{
+		return false;
+	}
+
+	public void setMaxUnitIncrement(int pixels)
+	{
+		this.maxUnitIncrement = pixels;
 	}
 
 }
--- Bingo Client/src/me/dylanmullen/bingo/game/chat/ChatPanel.java
+++ Bingo Client/src/me/dylanmullen/bingo/game/chat/ChatPanel.java
@@ -3,6 +3,10 @@ package me.dylanmullen.bingo.game.chat;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+import me.dylanmullen.bingo.window.ui.Button;
 import me.dylanmullen.bingo.window.ui.Panel;
 import me.dylanmullen.bingo.window.ui.UIColour;
 
@@ -16,24 +20,50 @@ public class ChatPanel extends Panel
 		super(x, y, width, height);
 	}
 
+	JTextArea input = new JTextArea();
+
 	@Override
 	public void setup()
 	{
 		setBounds(x, y, width, height);
 		setBackground(UIColour.FRAME_BINGO_BG_SIDE.toColor());
-		ChatComponent chat = new ChatComponent(15, 15, getWidth() - 30, getHeight() - 30);
-		chat.setWrapStyleWord(true);
-		add(chat);
 
-		addMouseListener(new MouseAdapter()
+		JScrollPane scroll = new JScrollPane();
+		ChatComponent chat = new ChatComponent(scroll,15, 15, getWidth() - 30, getHeight() - 45 - 50);
+		scroll.setViewportView(chat);
+		
+		scroll.setBounds(15, 15, getWidth() - 30, getHeight() - 45 - 50);
+		scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+		scroll.setBorder(null);
+		add(scroll);
+
+		Button button = new Button("Send", null, getWidth() - 15 - 100, getHeight() - 15 - 50, 100, 50,
+				UIColour.BTN_BINGO_ACTIVE);
+		button.create();
+
+		add(button);
+		input.setBounds(15, getHeight() - 15 - 50, getWidth() - 30 - 100, 50);
+		input.setWrapStyleWord(false);
+		input.setLineWrap(true);
+
+		add(input);
+
+		button.addMouseListener(new MouseAdapter()
 		{
 			@Override
 			public void mouseClicked(MouseEvent e)
 			{
-				String message = "(" + System.currentTimeMillis() + ") Test > sdfssdfsdfsfsfsfdfsfsdfsfdfsfsffdfsfdfsdffs";
-				chat.addMessage(message);
+				String message = "(" + System.currentTimeMillis()
+						+ ") Test > sdfssdfsdfsfsfsfdfsfsdfsfdfsfsffdfsfdfsdffs";
+				chat.addMessage(scroll, message);
 			}
 		});
+	}
+
+	private void update()
+	{
+
 	}
 
 	@Override
