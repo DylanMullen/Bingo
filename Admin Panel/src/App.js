1987ccd922ea46429751932cd6e5967ce9077c88
Dylan Mullen <31774413+DylanMullen@users.noreply.github.com>
2020-07-16 18:48:30 +0100

    Login Page Prototyped
    Login page has been prototyped.

--- Admin Panel/src/App.js
+++ Admin Panel/src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 import Navbar from './res/js/components/navbar';
 import Homepage from './res/js/pages/home';
 import UserPage from './res/js/pages/Users';
+import LoginPage from './res/js/pages/Login';
 
 function App() {
   return (
     <div className="app">
-      <Navbar />
-      <UserPage />
+      {/* <Navbar />
+      <UserPage /> */}
+      <LoginPage />
     </div>
   );
 }
--- Admin Panel/src/res/js/pages/Login.js
+++ Admin Panel/src/res/js/pages/Login.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Form, Card, Button } from 'react-bootstrap';
+import { Form, Card, Button, Container, Jumbotron } from 'react-bootstrap';
 
 export default class LoginPage extends React.Component {
     render() {
         return (
-            <section id="login">
-                <LoginForm />
-            </section>
+            <main id="login">
+                <Container fluid>
+                    <LoginForm />
+                </Container>
+            </main>
         );
     }
 }
@@ -14,21 +16,29 @@ export default class LoginPage extends React.Component {
 class LoginForm extends React.Component {
     render() {
         return (
-            <Card>
-                <Card.Body>
-                    <Form>
-                        <Form.Group>
-                            <Form.Label>Email Address</Form.Label>
-                            <Form.Control type="email" placeholder="Email" />
-                        </Form.Group>
-                        <Form.Group>
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
-                        </Form.Group>
-                        <Button variant="primary" type="submit" block>Login</Button>
-                    </Form>
-                </Card.Body>
-            </Card>
+            <section>
+                <Card className="shadow-lg">
+                    <Card.Body>
+                        <Jumbotron>
+                            <Container className="text-center">
+                                <h1>Please Login</h1>
+                                <p>You must login to proceed further</p>
+                            </Container>
+                        </Jumbotron>
+                        <Form>
+                            <Form.Group>
+                                <Form.Label>Email Address</Form.Label>
+                                <Form.Control type="email" placeholder="Email" />
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" placeholder="Password" />
+                            </Form.Group>
+                            <Button variant="primary" type="submit" block>Login</Button>
+                        </Form>
+                    </Card.Body>
+                </Card>
+            </section>
         );
     }
 
